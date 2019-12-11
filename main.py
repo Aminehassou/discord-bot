7372b8c9c2561a1d3c1f949cb01bb0f7b278a76b
Ahmed Amine Hassou <japnoo@gmail.com>
2019-12-11 01:14:27 +0100

    Setup the commands extension and optimised current commands

--- main.py
+++ main.py
@@ -1,44 +1,46 @@
-import discord
 import logging
+
+import discord
+from discord.ext import commands
 
 logging.basicConfig(level=logging.INFO)
 client = discord.Client()
 
-list = []
+bot = commands.Bot(command_prefix="$")
+botList = []
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
+@bot.command
+async def hello(ctx):
+    await ctx.send(ctx.message.content)
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    elif message.content.startswith("$list add "):
+    if message.content.startswith("$list add "):
         itemAdded = message.content[9:].strip() 
-        list.append(itemAdded)
+        botList.append(itemAdded)
         await message.channel.send("`{}` added to list.".format(itemAdded))
-    
+
     elif message.content.startswith("$list display"):
-        if len(list) != 0:
-            await message.channel.send("\n".join(["`{}. {}`".format(index + 1, anime) for index, anime in enumerate(list)]))
+        if len(botList) != 0:
+            await message.channel.send("\n".join(["`{}. {}`".format(index + 1, anime) for index, anime in enumerate(botList)]))
         else:
             await message.channel.send("The list is empty")
-    
+
     elif message.content.startswith("$list remove "):
         try:
             indexRemoved = int(message.content[12:].strip())
         except ValueError:
             indexRemoved = -1
         
-        if indexRemoved < 1 or indexRemoved > len(list):
+        if indexRemoved < 1 or indexRemoved > len(botList):
             await message.channel.send("That element does not exist")
         else:
-            itemRemoved = list.pop(indexRemoved - 1)
+            itemRemoved = botList.pop(indexRemoved - 1)
             await message.channel.send("Removed `{}` at index `{}`".format(itemRemoved, indexRemoved))
+    await bot.process_commands(message)
 
-client.run("NjQ0NzQ4OTYzNDU0NjQ4MzIw.Xc4nWw.mGPHYFltXUEXkwLNg7VzdJkG-WA")
+bot.run("NjQ0NzQ4OTYzNDU0NjQ4MzIw.Xc4nWw.mGPHYFltXUEXkwLNg7VzdJkG-WA")
