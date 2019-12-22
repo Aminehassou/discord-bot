8d31e51529baa613bf651e55cec762e373eb1486
Ahmed Amine Hassou <japnoo@gmail.com>
2019-12-22 07:46:24 +0100

    Finally fixed it, cogs work

---
+++ bot.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import discord
+from discord.ext import commands
+logging.basicConfig(level=logging.INFO)
+
+client = commands.Bot(command_prefix="$")
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"cogs.{filename[:-3]}")    
+
+
+client.run("NjQ0NzQ4OTYzNDU0NjQ4MzIw.Xc4nWw.mGPHYFltXUEXkwLNg7VzdJkG-WA")
