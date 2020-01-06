16c2d80e71b24ced896e9e17b3e4a4e12acb8f14
ahmed japnoo <japnoo@gmail.com>
2020-01-06 06:45:03 +0100

    Added leaderboard command that displays top 10 users

--- cogs/profile.py
+++ cogs/profile.py
@@ -3,15 +3,13 @@ import logging
 import discord
 from discord.ext import commands
 from collections import defaultdict
-from math import floor
+from collections import OrderedDict
 
 def calculateLevel(messageCount):
     result = 1 + messageCount/10
-    if result.is_integer():
-        return int(result)
-    else:
-        return int(floor(result))
+    return int(result)
 
+ 
 class Profile(commands.Cog):
     def __init__(self, client): 
         self.client = client
@@ -20,16 +18,32 @@ class Profile(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.author.bot:
-            self.userInfo[message.author.id]["Message Count"] += 1
-            if self.userInfo[message.author.id]["Level"] != calculateLevel(self.userInfo[message.author.id]["Message Count"]):
-                self.userInfo[message.author.id]["Level"] = calculateLevel(self.userInfo[message.author.id]["Message Count"])
-                await message.channel.send("Congratulations {}! You have reached **level {}**!".format(message.author.name, self.userInfo[message.author.id]["Level"]))
-
-            
+            currentId = message.author.id
+            currentName = message.author.name
+            newLevel = calculateLevel(self.userInfo[currentId]["messageCount"])
+            self.userInfo[currentId]["messageCount"] += 1
+            self.userInfo[currentId]["name"] = currentName
+            if self.userInfo[currentId]["level"] != newLevel:
+                self.userInfo[currentId]["level"] = newLevel
+                await message.channel.send("Congratulations {}! You have reached **level {}**!".format(message.author.name, self.userInfo[currentId]["level"]))
+    
     @commands.command()
     async def profile(self, ctx, member: discord.Member, channel: discord.TextChannel=None):
         channel = channel or ctx.channel
-        await ctx.send("**Username:** {}\n**Roles:** {}\n**Messages Sent:** {}\n **Level:** {}".format(member.display_name, ", ".join([role.name for role in member.roles[1:]]), self.userInfo[member.id]["Message Count"], self.userInfo[member.id]["Level"]))
-
+        await ctx.send("**Username:** {}\n**Roles:** {}\n**Messages Sent:** {}\n**Level:** {}".format(
+            member.display_name,
+            ", ".join([role.name for role in member.roles[1:]]),
+            self.userInfo[member.id]["messageCount"],
+            self.userInfo[member.id]["Level"])
+        )
+    @commands.command()
+    async def leaderboard(self, ctx):
+        #dict(sorted(d.items(), key = lambda x: x[1]["score"]))
+        board = sorted(self.userInfo.values(), key = lambda x: x["messageCount"])[:10]
+        output = ""
+        for index, user in enumerate(board):
+            output = output + "`{rank}. {name} ({count} messages)`\n".format(rank = index + 1, name = user["name"], count = user["messageCount"])
+        await ctx.send(output)
+        
 def setup(client):
     client.add_cog(Profile(client))
