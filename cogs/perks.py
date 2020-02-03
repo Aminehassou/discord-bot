68c8b3cc62303228625624fa4c448c7cdcf58e0d
ahmed japnoo <japnoo@gmail.com>
2020-02-03 01:40:47 +0100

    Added working perks

--- cogs/perks.py
+++ cogs/perks.py
@@ -10,12 +10,13 @@ class Perks(commands.Cog):
         self.client = client
         self.roleMessage = ""
         self.emoteClasses = {"Mage": "1️⃣", "Warrior": "2️⃣", "Archer": "3️⃣"}
+        self.emotePerks = {"Damage_up": "⚔️", "Defense_up": "🛡️", "Health_up": "❤️"}
 
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         mssgId = payload.message_id
-        if mssgId == self.roleMessage.id:
+        if mssgId == self.roleMessage.id and payload.user_id != 644748963454648320:
             print(self.roleMessage.id)
             guildId = payload.guild_id
             guild = discord.utils.find(lambda currentGuild: currentGuild.id == guildId, self.client.guilds)
@@ -25,6 +26,15 @@ class Perks(commands.Cog):
                 if payload.emoji.name == value:
                     role = discord.utils.get(guild.roles, name=key)
 
+            for key, value in self.emotePerks.items():
+                if payload.emoji.name == value:
+                    role = discord.utils.get(guild.roles, name=key)
+                    await self.roleMessage.delete()
+
+            if payload.emoji.name == "4️⃣":
+                    await self.roleMessage.delete()
+                    role = None
+            
             if role is not None:
                 member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
                 if member is not None:
@@ -61,19 +71,22 @@ class Perks(commands.Cog):
 
     @commands.command()
     async def upgrade(self, ctx):
-        """Displays currently available classes"""
+        """Displays currently available perks/classes"""
         currentUser = ctx.author.name
         user = db.getUser(ctx.author.id, ctx.author.guild.id)
         if user["level"] % 3 != 0:
             self.roleMessage = await ctx.send("You're not a high enough level yet!")
+        
         elif user["level"] == 3:
-            self.roleMessage = await ctx.send("React with one of the emotes corresponding to the class you want, {}:\n:one: : Mage\n:two: : Warrior\n:three: : Archer".format(currentUser))
-            await self.roleMessage.add_reaction("1️⃣")
-            await self.roleMessage.add_reaction("2️⃣")
-            await self.roleMessage.add_reaction("3️⃣")
-        elif user["level"] % 3 == 0 and user["level"] > 3:
-            self.roleMessage = await ctx.send("React with one of the emotes corresponding to the perk you want, {}:".format(currentUser))
+            self.roleMessage = await ctx.send("React with one of the emotes corresponding to the class you want, {}:\n:one: : Mage\n:two: : Warrior\n:three: : Archer\n:four: : Exit".format(currentUser))
+            for key, value in self.emoteClasses.items():
+                await self.roleMessage.add_reaction(value)
+            await self.roleMessage.add_reaction("4️⃣")
 
+        elif user["level"] % 3 == 0 and user["level"] > 3:
+            self.roleMessage = await ctx.send("React with one of the emotes corresponding to the perk you want, {}:\n⚔️ : Damage up\n🛡️ : Defense up\n❤️ : Health up".format(currentUser))
+            for key, value in self.emotePerks.items():
+                await self.roleMessage.add_reaction(value)
 
 def setup(client):
     client.add_cog(Perks(client))
