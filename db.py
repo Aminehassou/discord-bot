746fbcd74c457c9c2293918b9e843070bdbde7d9
ahmed japnoo <japnoo@gmail.com>
2020-05-12 06:00:55 +0000

    Finished main impelementation for adventure command

--- db.py
+++ db.py
@@ -12,21 +12,25 @@ def getUser(userId, guildId):
     return c.fetchone()
 
 def insertNewUser(userId, guildId, name):
-    newUser = {"id": userId, "guild_id": guildId, "name": name, "messages": 0, "level": 0, "is_upgraded": 0}
-    c.execute("INSERT INTO users VALUES (:id, :guild_id, :name, :messages, :level, :is_upgraded)", newUser)
+    newUser = {"id": userId, "guild_id": guildId, "name": name, "messages": 0, "level": 0, "is_upgraded": 0, "currency": 0}
+    c.execute("INSERT INTO users VALUES (:id, :guild_id, :name, :messages, :level, :is_upgraded, :currency)", newUser)
     conn.commit()
     return newUser
 
 def updateUser(user):
-    c.execute("UPDATE users SET name = :name, messages = :messages, level = :level WHERE id = :id AND guild_id = :guild_id", user)
+    c.execute("UPDATE users SET name = :name, messages = :messages, level = :level, currency = :currency WHERE id = :id AND guild_id = :guild_id", user)
     conn.commit()
 
 def updateUpgradeStatus(userId, guildId, newStatus):
     c.execute("update users SET is_upgraded = :is_upgraded WHERE id = :id AND guild_id = :guild_id", {"is_upgraded": newStatus, "id": userId, "guild_id": guildId})
     conn.commit()
 
-def updateLevel(userId, guildId, newLevel):
-    c.execute("update users SET level = :newLevel WHERE id = :id AND guild_id = :guild_id", {"newLevel": newLevel, "id": userId, "guild_id": guildId})
+def modifyLevel(userId, guildId, increment):
+    c.execute("update users SET currency = currency + :increment WHERE id = :id AND guild_id = :guild_id", {"increment": increment, "id": userId, "guild_id": guildId})
+    conn.commit()
+
+def modifyCurrency(userId, guildId, increment):
+    c.execute("update users SET currency = currency + :increment WHERE id = :id AND guild_id = :guild_id", {"increment": increment, "id": userId, "guild_id": guildId})
     conn.commit()
 
 def sortByMessages(limit, guildId):
